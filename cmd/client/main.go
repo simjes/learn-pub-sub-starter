bdbd4f551168eb2ab07a0e1433e23ca458b4b0be
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2024-05-25 13:04:52 +0200

    ch5 ✅

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -37,8 +37,9 @@ func main() {
 
 	gameState := gamelogic.NewGameState(username)
 
-	pubsub.SubscribeJSON(connection, routing.ExchangePerilDirect, routing.PauseKey+"."+username, routing.PauseKey, pubsub.Transient, handlerPause(gameState))                  // pubsub.Direct,
-	pubsub.SubscribeJSON(connection, routing.ExchangePerilTopic, routing.ArmyMovesPrefix+"."+username, routing.ArmyMovesPrefix+".*", pubsub.Transient, handlerMove(gameState)) // pubsub.Topic,
+	pubsub.SubscribeJSON(connection, routing.ExchangePerilDirect, routing.PauseKey+"."+username, routing.PauseKey, pubsub.Transient, handlerPause(gameState))
+	pubsub.SubscribeJSON(connection, routing.ExchangePerilTopic, routing.ArmyMovesPrefix+"."+username, routing.ArmyMovesPrefix+".*", pubsub.Transient, handlerMove(channel, gameState))
+	pubsub.SubscribeJSON(connection, routing.ExchangePerilTopic, routing.WarRecognitionsPrefix, routing.WarRecognitionsPrefix+".*", pubsub.Durable, handlerWar(gameState))
 
 	for {
 		userInput := gamelogic.GetInput()
@@ -103,15 +104,58 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.Ack
 	}
 }
 
-func handlerMove(gs *gamelogic.GameState) func(move gamelogic.ArmyMove) pubsub.AckType {
+func handlerMove(ch *amqp.Channel, gs *gamelogic.GameState) func(move gamelogic.ArmyMove) pubsub.AckType {
 	return func(mv gamelogic.ArmyMove) pubsub.AckType {
 		defer fmt.Print("> ")
 		outcome := gs.HandleMove(mv)
 
-		if outcome == gamelogic.MoveOutComeSafe || outcome == gamelogic.MoveOutcomeMakeWar {
+		if outcome == gamelogic.MoveOutcomeMakeWar {
+			err := pubsub.PublishJSON(ch, routing.ExchangePerilTopic, routing.WarRecognitionsPrefix+"."+gs.Player.Username, gamelogic.RecognitionOfWar{
+				Attacker: mv.Player,
+				Defender: gs.Player,
+			})
+
+			if err != nil {
+				return pubsub.NackRequeue
+			}
+
 			return pubsub.Ack
 		}
 
+		if outcome == gamelogic.MoveOutComeSafe {
+			return pubsub.Ack
+		}
+
+		return pubsub.NackDiscard
+	}
+}
+
+func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.AckType {
+	return func(rw gamelogic.RecognitionOfWar) pubsub.AckType {
+		defer fmt.Print("> ")
+		outcome, _, _ := gs.HandleWar(rw)
+
+		if outcome == gamelogic.WarOutcomeNotInvolved {
+			return pubsub.NackRequeue
+		}
+
+		if outcome == gamelogic.WarOutcomeNoUnits {
+			return pubsub.NackDiscard
+		}
+
+		if outcome == gamelogic.WarOutcomeOpponentWon {
+			return pubsub.Ack
+		}
+
+		if outcome == gamelogic.WarOutcomeYouWon {
+			return pubsub.Ack
+		}
+
+		if outcome == gamelogic.WarOutcomeDraw {
+			return pubsub.Ack
+		}
+
+		fmt.Printf("Error handling outcome, %v", outcome)
 		return pubsub.NackDiscard
 	}
 }
